4a7537aeed0aa9e6d515de644d554895481e7305
Yekiaz <saadk200531@gmail.com>
2026-01-23 00:39:03 +0530

    Update sales_analysis.sql

--- SQL_Ecommerce_Sales_Analysis/sales_analysis.sql
+++ SQL_Ecommerce_Sales_Analysis/sales_analysis.sql
@@ -1,3 +1,27 @@
 -- SQL E-commerce Sales Analysis
 -- Author: Yekiaz
 -- Description: SQL queries to analyze e-commerce sales data
+
+USE ecommerce_db;
+
+-- Preview data
+SELECT *
+FROM ecommerce_sales_data
+LIMIT 10;
+
+-- Total revenue by product category
+SELECT
+    ProductCategory,
+    ROUND(SUM(Quantity * Price), 2) AS total_sales
+FROM ecommerce_sales_data
+GROUP BY ProductCategory
+ORDER BY total_sales DESC;
+
+-- Top 10 products by revenue
+SELECT
+    ProductID,
+    ROUND(SUM(Quantity * Price), 2) AS revenue
+FROM ecommerce_sales_data
+GROUP BY ProductID
+ORDER BY revenue DESC
+LIMIT 10;
